dcf2c73bcf1d3d533277b76f22a6717a2f4771f3
G91688 <gultacmansimova0493@gmail.com>
2022-10-28 18:43:04 +0200

    doubleclick

---
+++ js/ü.js
@@ -0,0 +1,9 @@
+function activchat(){
+    document.getElementById('chatbox').style.left ="0px"
+}
+   
+   
+    function deactivatechat(){
+   
+    document.getElementById('chatbox').style.left ="-400px"
+}
